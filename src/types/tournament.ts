25d127c136fec918c32d254041319a6a99108bf3
vader066 <humphreydonkor066@gmail.com>
2026-01-10 20:57:04 +0000

    feat: updated tournament types after tournament db migrations

--- src/types/tournament.ts
+++ src/types/tournament.ts
@@ -5,6 +5,8 @@ export interface TOURNAMENT {
   id: string
   tournamentName: string
   numberOfRounds: number
+  numberOfGames: number
+  status: 'in_progress' | 'completed' | 'cancelled'
   games: Array<GAME>
   playerIDs: Array<string> // player id's
   dbPlayers?: Array<PLAYER> // don't use for now
@@ -18,6 +20,8 @@ export interface DBTourney {
   tournament_name: string
   number_of_rounds: number
   number_of_players: number
+  number_of_games: number
+  status: 'in_progress' | 'completed' | 'cancelled'
   synced: boolean
   club_id: string
   began_at?: string
